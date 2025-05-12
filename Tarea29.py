1e27b90e01d322d7f10a12997dd71e6eb839588b
Pablo <Pabloptandazo1624@gmail.com>
2025-05-12 12:17:14 -0500

    Tarea 29

--- Tarea29.py
+++ Tarea29.py
@@ -1,11 +1,43 @@
 #Sumar números ingresados por el usuario hasta que ingrese 0.
-usuario1= int(input("Ingrese su primer numero: "))
-usuario2=int(input("Ingrese su segundo numero: "))
+print("1. Sumar números hasta 0:")
+suma = 0
+numero = int(input("Ingresa un número (0 para terminar): "))
+while numero != 0:
+    suma += numero
+    numero = int(input("Ingresa otro número (0 para terminar): "))
+print("La suma total es:", suma)
+   
+
+
+    
 #Adivinar un número aleatorio entre 1 y 100 (pistas: "mayor" o "menor").
 
-#Validar contraseña (repetir hasta que coincida con una guardada).
+import random
+numeroSecreto = random.randint(1,100)
+intento=None
 
+while intento != numeroSecreto:
+    intento=int(input("Adivina el numero del 1 al 100: "))
+    if intento < numeroSecreto:
+        print("Mayor")
+
+    elif intento > numeroSecreto:
+        print("Menor")
+
+    else:
+        print("Muy bien el numero secreo es: ", numeroSecreto)
+
+#Validar contraseña (repetir hasta que coincida con una guardada).
+Contra = "Pablo"
+while True:
+    Ingresa = input("Ingresa la contraseña")
+    if Ingresa== Contra:
+        print("La contraseña es correcta: ")
+
+    else:
+        print("La contraseña es incorrecta: intenta nuevamente")
 #Simular un cajero automático (menú: retirar, depositar, salir).
+
 
 #Calcular la raíz cuadrada por aproximación (método babilónico).
 
